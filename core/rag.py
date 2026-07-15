3c72709fc44964a8281df17e606787dadb2f9be5
makss738 <maxence.monka@cyberv.fr>
2026-07-15 14:40:07 +0200

    Create rag.py

---
+++ core/rag.py
@@ -0,0 +1,73 @@
+import os
+
+from llama_index.core import VectorStoreIndex, SimpleDirectoryReader
+
+
+KNOWLEDGE_PATH = "data/knowledge"
+
+
+
+def load_knowledge():
+
+    documents = SimpleDirectoryReader(
+        KNOWLEDGE_PATH,
+        recursive=True
+    ).load_data()
+
+
+    return documents
+
+
+
+def create_index():
+
+    documents = load_knowledge()
+
+
+    index = VectorStoreIndex.from_documents(
+        documents
+    )
+
+
+    return index
+
+
+
+def ask_question(question):
+
+    index = create_index()
+
+
+    engine = index.as_query_engine()
+
+
+    response = engine.query(
+        question
+    )
+
+
+    return response
+
+
+
+if __name__ == "__main__":
+
+
+    print(
+        "🧠 CTI RAG Assistant"
+    )
+
+
+    question = input(
+        "Question : "
+    )
+
+
+    answer = ask_question(
+        question
+    )
+
+
+    print("\nRéponse :")
+
+    print(answer)
